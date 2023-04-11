b171d7dd06e99c3cd3e1fea448bb9a0f63cea416
Sergiy Nikolaev <122089422+fsdev3@users.noreply.github.com>
2023-04-11 19:18:05 +0200

    objects 1

--- js/objects.js
+++ js/objects.js
@@ -103,18 +103,44 @@
 //   d: 5,
 // };
 // console.log(Object.keys(obj).length);
-const apartment = {
-  imgUrl: 'https://via.placeholder.com/640x480',
-  descr: 'Spacious apartment in the city center',
-  rating: 4,
-  price: 2153,
-  tags: ['premium', 'promoted', 'top'],
-  owner: {
-    name: 'Henry',
-    phone: '982-126-1588',
-    email: 'henry.carter@aptmail.com',
-  },
-};
 
-const lastTag = apartment.tags[apartment.tags.length - 1];
-console.log(lastTag);
+// iterate object and create 2 arrays with keys and values
+// const apartment = {
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+// };
+// const keys = [];
+// const values = [];
+// for (const key in apartment) {
+//   keys.push(key);
+//   values.push(apartment[key]);
+// }
+// console.log(keys);
+// console.log(values);
+
+// count quantity of Own Property
+// function countProps(object) {
+//   let propCount = 0;
+//   for (let key in object) {
+//     if (object.hasOwnProperty(key)) {
+//       propCount += 1;
+//     }
+//   }
+//   return propCount;
+// }
+
+// get arrays keys and values
+// const apartment = {
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+// };
+// const values = [];
+// // Change code below this line
+// const keys = Object.keys(apartment);
+// for (let key of keys) {
+//   values.push(apartment[key]);
+// }
+// console.log([keys]);
+// console.log([values]);
