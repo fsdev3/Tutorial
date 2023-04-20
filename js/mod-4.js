a5c539a1b1eca56d1f7eede8f7eb793ab2ea8b59
Sergiy Nikolaev <122089422+fsdev3@users.noreply.github.com>
2023-04-20 16:15:58 +0200

    m4 ok

--- js/mod-4.js
+++ js/mod-4.js
@@ -254,12 +254,62 @@
 //     gender: 'female',
 //     age: 34,
 //   },
+//   {
+//     name: 'Ross Vazquez',
+//     email: 'rossvazquez@xinware.com',
+//     eyeColor: 'green',
+//     friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+//     isActive: false,
+//     balance: 3793,
+//     gender: 'male',
+//     age: 24,
+//   },
+//   {
+//     name: 'Elma Head',
+//     email: 'elmahead@omatom.com',
+//     eyeColor: 'green',
+//     friends: ['Goldie Gentry', 'Aisha Tran'],
+//     isActive: true,
+//     balance: 2278,
+//     gender: 'female',
+//     age: 21,
+//   },
+//   {
+//     name: 'Carey Barr',
+//     email: 'careybarr@nurali.com',
+//     eyeColor: 'blue',
+//     friends: ['Jordan Sampson', 'Eddie Strong'],
+//     isActive: true,
+//     balance: 3951,
+//     gender: 'male',
+//     age: 27,
+//   },
+//   {
+//     name: 'Blackburn Dotson',
+//     email: 'blackburndotson@furnigeer.com',
+//     eyeColor: 'brown',
+//     friends: ['Jacklyn Lucas', 'Linda Chapman'],
+//     isActive: false,
+//     balance: 1498,
+//     gender: 'male',
+//     age: 38,
+//   },
+//   {
+//     name: 'Sheree Anthony',
+//     email: 'shereeanthony@kog.com',
+//     eyeColor: 'brown',
+//     friends: ['Goldie Gentry', 'Briana Decker'],
+//     isActive: true,
+//     balance: 2764,
+//     gender: 'female',
+//     age: 39,
+//   },
 // ];
 
 // const getFriends = users =>
 //   users
 //     .flatMap(user => user.friends)
-//     .filter((friend, index, array) => array.indexOf(friend) === index); // V2 - one chain
+// .filter((friend, index, array) => array.indexOf(friend) === index); // V2 - one chain
 
 // const allFriends = users.flatMap(user => user.friends); // V1
 // const getFriends = (users, allFriends) =>
@@ -267,3 +317,15 @@
 
 // console.log(allFriends);
 // console.log(getFriends);
+
+// const getActiveUsers = users =>
+//   users
+//     .flatMap(user => user.isActive)
+//     .filter((user, index, array) => array.isActive === true);
+
+// zad 26
+// const getActiveUsers = users => users.filter(user => user.isActive); // true or !user.isActive - false array
+// console.log(getActiveUsers(users));
+
+// console.log(names(book.rating));
+// console.log(getNamesSortedByFriendCount(users));
