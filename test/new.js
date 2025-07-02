d2b3e52b15e91ce32bfc998ae04fa23f230ec035
Sergiy Nikolaev <122089422+fsdev3@users.noreply.github.com>
2025-07-02 19:23:45 +0200

    Update new.js n1

--- test/new.js
+++ test/new.js
@@ -4,12 +4,15 @@ const validateEmail = email => {
   return validator.isEmail(email);
 };
 
-console.log(
-  'Is mango@mail.com a valid email?: ',
-  validateEmail('mango@mail.com')
-);
+
 
 console.log(
   'Is Mangozedog.com a valid email?: ',
   validateEmail('Mangozedog.com')
+);
+
+
+console.log(
+  'Is mango@mail.com a valid email?: ',
+  validateEmail('mango@mail.com')
 );
